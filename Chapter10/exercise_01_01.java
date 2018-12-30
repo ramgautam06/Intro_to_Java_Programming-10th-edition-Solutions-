d7029fc99dc42acb037c3648b2594f0076446823
Ram Gautam <43186153+ramgautam06@users.noreply.github.com>
2018-12-30 16:27:15 -0500

    Add files via upload

---
+++ Chapter10/exercise_01_01.java
@@ -0,0 +1,9 @@
+public class exercise_01_01
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome to Java.");
+        System.out.println("Welcome to Computer Science.");
+        System.out.println("Programming is fun.");
+    }
+}
