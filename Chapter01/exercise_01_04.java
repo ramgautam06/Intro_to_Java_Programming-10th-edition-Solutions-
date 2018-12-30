a5b0217cb5a226bce97ba6e0eead082d7c305f01
Ram Gautam <43186153+ramgautam06@users.noreply.github.com>
2018-12-30 16:28:14 -0500

    Add files via upload

---
+++ Chapter01/exercise_01_04.java
@@ -0,0 +1,11 @@
+public class exercise_01_04
+{
+    public static void main(String[] args)
+    {
+        System.out.println("a  " + "a^2  " + "a^3");
+        for(int i = 1;i <=4;i++)
+        {
+            System.out.println(i + "   " + i*i + "   " + i*i*i);
+        }
+    }
+}
